1deaab617f0666a9605b8d2e9f659894cde9d370
Chip Cerio <alistair.cerio@gmail.com>
2025-08-31 15:39:31 +0800

    Update team avatar

--- components/Team.tsx
+++ components/Team.tsx
@@ -10,30 +10,30 @@ interface TeamMember {
 
 const teamMembers: TeamMember[] = [
   {
-    name: 'Jett Russo',
+    name: 'JP',
     image:
       'https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?ixlib=rb-4.0.3&auto=format&fit=crop&w=400&q=80',
   },
   {
-    name: 'Evelyn Hunter',
-    image:
-      'https://images.unsplash.com/photo-1544005313-94ddf0286df2?ixlib=rb-4.0.3&auto=format&fit=crop&w=400&q=80',
-  },
-  {
-    name: 'Kenia Fischer',
-    image:
-      'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-4.0.3&auto=format&fit=crop&w=400&q=80',
-  },
-  {
-    name: 'Miguel',
-    image:
-      'https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-4.0.3&auto=format&fit=crop&w=400&q=80',
-  },
-  {
-    name: 'Scott',
+    name: 'Chip',
     image:
       'https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixlib=rb-4.0.3&auto=format&fit=crop&w=400&q=80',
   },
+  {
+    name: 'Shane',
+    image:
+      'https://images.unsplash.com/photo-1599566150163-29194dcaad36?ixlib=rb-4.0.3&auto=format&fit=crop&w=400&q=80',
+  },
+  {
+    name: 'Jocker',
+    image:
+      'https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-4.0.3&auto=format&fit=crop&w=400&q=80',
+  },
+  // {
+  //   name: 'Scott',
+  //   image:
+  //     'https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixlib=rb-4.0.3&auto=format&fit=crop&w=400&q=80',
+  // },
   // { name: 'Evan Bell', role: 'Head of Design', image: 'https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-4.0.3&auto=format&fit=crop&w=400&q=80', hasLinkedIn: true },
   // { name: 'Sierra Joseph', image: 'https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-4.0.3&auto=format&fit=crop&w=400&q=80' },
   // { name: 'Enzo Brigs', image: 'https://images.unsplash.com/photo-1599566150163-29194dcaad36?ixlib=rb-4.0.3&auto=format&fit=crop&w=400&q=80' },
@@ -47,10 +47,10 @@ export default function Team() {
           {/* Header */}
           <div className='mb-12'>
             <h2 className='text-4xl md:text-5xl font-light text-white mb-4'>
-              Lorem ipsum dolor sit amet consectetur.
+              Meet the team behind PickAxe
             </h2>
             <p className='text-white/80 text-sm'>
-              Pellentesque congue justo sed ex pretium, vitae ornare lorem hendrerit.
+              A passionate team dedicated to connecting skilled workers with those who need their expertise.
             </p>
           </div>
 
